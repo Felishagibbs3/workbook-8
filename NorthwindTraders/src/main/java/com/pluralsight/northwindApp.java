221ac7a5bd199c97a7bd031bf7b571b3861f9063
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-06-10 08:24:09 -0500

    Exercise for database connection complete

--- NorthwindTraders/src/main/java/com/pluralsight/northwindApp.java
+++ NorthwindTraders/src/main/java/com/pluralsight/northwindApp.java
@@ -5,17 +5,27 @@ import java.sql.*;
 public class northwindApp {
 
     public static void main(String[] args) {
+        String username = "root";
+        String password = "Yearup2025!!";
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "Yearup2025!!");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
-            String query = "SELECT * FROM products";
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery(query);
+
+            PreparedStatement statement = connection.prepareStatement("SELECT productID, productName, UnitPrice, Unitsinstock FROM products");
+            ResultSet results = statement.executeQuery();
 
             while (results.next()) {
-                String products = results.getString("productName");
-                System.out.println(products);
+                int id = results.getInt("ProductID");
+                String name = results.getString("ProductName");
+                double price = results.getDouble("UnitPrice");
+                int stock = results.getInt("UnitsInStock");
+
+                System.out.println("Product ID: " + id);
+                System.out.println("Product Name: " + name);
+                System.out.println("Unit Price: " + price);
+                System.out.println("Units in Stock: " + stock);
+                System.out.println("------------------------------------");
             }
             connection.close();
         } catch (SQLException e) {
@@ -25,3 +35,7 @@ public class northwindApp {
     }
 
 }
+
+//continue working on the NorthwindTraders in the previous exercise.
+//Modify your code to display all Products in the Northwind database. The list
+//must include: - product id - product name - unit price - units in stock
